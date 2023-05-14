c7317622ed3baf239ea07fe026c38919b5518683
Amogh Ghadge <amoghghadge22@gmail.com>
2023-05-14 00:01:49 -0400

    Extracting audio features for songs on top charts

--- data_collection.py
+++ data_collection.py
@@ -4,26 +4,34 @@ import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-spotify_data = pd.read_csv('./src/sagemaker/data/tracks_features.csv')
+# remove duplicates in charts data (done only once)
+# spotify_charts = pd.read_csv('./src/sagemaker/data/charts.csv')
+# print(spotify_charts.head(10))
+# spotify_charts.info()
+# spotify_charts = spotify_charts.drop_duplicates(subset="title")
+# spotify_charts.to_csv('./src/sagemaker/data/charts_no_duplicates.csv', encoding='utf-8', index=False)
+
+spotify_charts = pd.read_csv("./src/sagemaker/data/charts_no_duplicates.csv")
+print(spotify_charts.head(5))
+spotify_charts.info()
+
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=os.environ["SPOTIFY_CLIENT_ID"], client_secret=os.environ["SPOTIFY_CLIENT_SECRET"]))
 
-#for i in range(3):
-#name = spotify_data.iloc[i]['name']
-#year = spotify_data.iloc[i]['year']
+# find audio features for first 3 songs
+for i in range(3):
+    name = spotify_charts.iloc[i]['title']
+    year = spotify_charts.iloc[i]['date'][:4]
+    artists = spotify_charts.iloc[i]['artist']
 
-name = "if looks could kill"
-year = 2023
-    
-results = sp.search(q= 'track: {} year: {}'.format(name, year), limit=1)
-print(results)
-results = results['tracks']['items'][0]
-    
-track_id = results['id']
-audio_features = sp.audio_features(track_id)[0]
-print()
-print("AUDIO FEATURES OF: " + name)
+    # returns dictionary, with many keys having other dictionaries as values
+    results = sp.search(q= 'track: {} artist: {}'.format(name, artists), limit=1)
+    uri = results['tracks']['items'][0]['uri']
 
-print()
+    # returns list of dictionaries, get the first one
+    audio_features = sp.audio_features(uri)[0]
 
-for key, value in audio_features.items():
-    print(str(key) + " " + str(value))
+    # add audio features as columns to song's row
+    for key, value in audio_features.items():
+        spotify_charts.iloc[i][key] = value
+
+spotify_charts.to_csv("./src/sagemaker/data/charts_with_audio_features.csv")

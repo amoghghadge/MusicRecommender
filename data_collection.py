08d565b49b6c5c54af83ece304c5bb08c2655968
Amogh Ghadge <amoghghadge22@gmail.com>
2023-05-14 00:31:34 -0400

    Fixed bugs writing to csv

--- data_collection.py
+++ data_collection.py
@@ -16,9 +16,11 @@ print(spotify_charts.head(5))
 spotify_charts.info()
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=os.environ["SPOTIFY_CLIENT_ID"], client_secret=os.environ["SPOTIFY_CLIENT_SECRET"]))
+df = pd.DataFrame(columns=['name', 'year', 'artists', 'track_uri', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 
+                           'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms', 'time_signature'])
 
-# find audio features for first 3 songs
-for i in range(3):
+# find audio features for all songs on spotify charts
+for i in range(len(spotify_charts.index)):
     name = spotify_charts.iloc[i]['title']
     year = spotify_charts.iloc[i]['date'][:4]
     artists = spotify_charts.iloc[i]['artist']
@@ -30,8 +32,15 @@ for i in range(3):
     # returns list of dictionaries, get the first one
     audio_features = sp.audio_features(uri)[0]
 
-    # add audio features as columns to song's row
-    for key, value in audio_features.items():
-        spotify_charts.iloc[i][key] = value
+    # see audio features of track
+    # print("\nAUDIO FEATURES OF: " + name)
+    # print()
+    # for key, value in audio_features.items():
+    #     print(str(key) + " " + str(value))
 
-spotify_charts.to_csv("./src/sagemaker/data/charts_with_audio_features.csv")
+    # add audio features as columns to song's row
+    relevant_features = [audio_features[col] for col in df.columns if col not in ['name', 'year', 'artists', 'track_uri']]
+    row = [name, year, artists, uri, *relevant_features]
+    df.loc[len(df.index)] = row
+
+df.to_csv("./src/sagemaker/data/charts_with_audio_features.csv", encoding='utf-8', index=False)
